6094d600becaba051091d44c1fb1c50a90077dda
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-11-19 12:50:57 +0800

    Upadte main.go

--- cqhttp/main.go
+++ cqhttp/main.go
@@ -1,9 +1,12 @@
 package cqhttp
 
+var FirstStart bool = true
+
 var XQPath = PathExecute()
 var AppPath = XQPath + "data/app/onebot-yaya/"
 var ImagePath = XQPath + "data/image/onebot-yaya/"
 var RecordPath = XQPath + "data/record/onebot-yaya/"
+var VideoPath = XQPath + "data/video/onebot-yaya/"
 
 func init() {
 }
@@ -12,15 +15,24 @@ func Main() {
 }
 
 func OnStart() {
-	CreatePath(AppPath)
-	CreatePath(ImagePath)
-	CreatePath(RecordPath)
-	INFO("夜夜は世界一かわいい")
-	Conf = Load(AppPath + "config.yml")
-	if Conf == nil {
-		ERROR("晚安~")
-		return
+	if FirstStart {
+		CreatePath(AppPath)
+		CreatePath(ImagePath)
+		CreatePath(RecordPath)
+		CreatePath(VideoPath)
+		INFO("夜夜は世界一かわいい")
+		Conf = Load(AppPath + "config.yml")
+		if Conf == nil {
+			ERROR("晚安~")
+			return
+		}
+		WSCInit(Conf)
+
+		go WSCStarts()
 	}
-	WSCInit(Conf)
-	go WSCStarts()
+	FirstStart = false
+}
+
+func OnDisable() {
+	//
 }
