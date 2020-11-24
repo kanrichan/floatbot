3784e02af5a1651853bf2711edd3181956301f30
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-11-25 01:22:51 +0800

    Update api.go

--- cqhttp/api.go
+++ cqhttp/api.go
@@ -1,6 +1,7 @@
 package cqhttp
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/base64"
 	"encoding/json"
@@ -8,24 +9,57 @@ import (
 	"os"
 	"strings"
 
+	xml "encoding/xml"
 	"github.com/tidwall/gjson"
 
 	"yaya/core"
 )
 
-type Data map[string]interface{}
-type Echo struct {
-	Seq int64 `json:"seq"`
-}
-
 type Result struct {
-	Status  string `json:"status"`
-	Retcode int64  `json:"retcode"`
-	Data    Data   `json:"data"`
-	Echo    Echo   `json:"echo"`
+	Status  string      `json:"status"`
+	Retcode int64       `json:"retcode"`
+	Data    interface{} `json:"data"`
+	Echo    interface{} `json:"echo"`
 }
 
 type Reply []map[string]interface{}
+
+type XQGroupMemberList struct {
+	List []*XQGroupMembers `json:"list"`
+}
+type XQGroupMembers struct {
+	QQ  int64 `json:"QQ"`
+	Lv  int64 `json:"lv"`
+	Val int64 `json:"val"`
+}
+type CQGroupMember struct {
+	GroupID         int64  `json:"group_id"`
+	UserID          int64  `json:"user_id"`
+	Nickname        string `json:"nickname"`
+	Card            string `json:"card"`
+	Sex             string `json:"sex"`
+	Age             int64  `json:"age"`
+	Area            string `json:"area"`
+	JoinTime        int64  `json:"join_time"`
+	LastSentTime    int64  `json:"last_sent_time"`
+	Level           string `json:"level"`
+	Role            string `json:"role"`
+	Unfriendly      bool   `json:"unfriendly"`
+	Title           string `json:"title"`
+	TitleExpireTime int64  `json:"title_expire_time"`
+	CardChangeable  bool   `json:"card_changeable"`
+}
+type CQGroupInfo struct {
+	GroupID        int64  `json:"group_id"`
+	GroupName      string `json:"group_name"`
+	MemberCount    int64  `json:"member_count"`
+	MaxMemberCount int64  `json:"max_member_count"`
+}
+type CQFriendInfo struct {
+	UserID   int64  `json:"user_id"`
+	Nickname string `json:"nickname"`
+	Remark   string `json:"remark"`
+}
 
 func (c *WSC) WSCApi() {
 	defer func() {
@@ -44,20 +78,28 @@ func (c *WSC) WSCApi() {
 		req := gjson.ParseBytes(api)
 		action := strings.ReplaceAll(req.Get("action").Str, "_async", "")
 		params := req.Get("params")
-		echo := req.Get("echo.seq").Int()
 
 		DEBUG("[响应服务] Bot %v 接收到API调用: %v 参数: %v", c.Bot, req.Get("action").Str, string(api))
 		if f, ok := wsApi[action]; ok {
 			ret := f(c.Bot, params)
-			ret.Echo.Seq = echo
+			if req.Get("echo").Str != "" {
+				ret.Echo = req.Get("echo").Str
+			} else {
+				ret.Echo, _ = req.Get("echo").Value().(map[string]interface{})
+			}
 			send, _ := json.Marshal(ret)
-
 			c.Send <- send
 		} else {
-			ret := resultFail(Data{})
-			ret.Echo.Seq = echo
+			ret := resultFail("no such api")
+			TEST("%v", ret)
+			if req.Get("echo").Str != "" {
+				ret.Echo = req.Get("echo").Str
+			} else {
+				ret.Echo, _ = req.Get("echo").Value().(map[string]interface{})
+			}
+			TEST("%v", ret)
 			send, _ := json.Marshal(ret)
-
+			TEST("%v", send)
 			c.Send <- send
 		}
 	}
@@ -93,35 +135,35 @@ var wsApi = map[string]func(int64, gjson.Result) Result{
 		return SendMessage(bot, 2, group_id, 0, messages)
 	},
 	"delete_msg": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_msg": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_forward_msg": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "先驱好像不支持"})
+		return resultFail(map[string]interface{}{"data": "先驱好像不支持"})
 	},
 	"send_like": func(bot int64, p gjson.Result) Result {
 		user_id := p.Get("user_id").Int()
 		core.UpVote(bot, user_id)
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_kick": func(bot int64, p gjson.Result) Result {
 		group_id := p.Get("group_id").Int()
 		user_id := p.Get("user_id").Int()
 		reject_add_request := p.Get("reject_add_request").Bool()
 		core.KickGroupMBR(bot, group_id, user_id, reject_add_request)
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_ban": func(bot int64, p gjson.Result) Result {
 		group_id := p.Get("group_id").Int()
 		user_id := p.Get("user_id").Int()
 		duration := p.Get("duration").Int()
 		core.ShutUP(bot, group_id, user_id, duration)
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_anonymous_ban": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"set_group_whole_ban": func(bot int64, p gjson.Result) Result {
 		group_id := p.Get("group_id").Int()
@@ -131,125 +173,196 @@ var wsApi = map[string]func(int64, gjson.Result) Result{
 		} else {
 			core.ShutUP(bot, group_id, 0, 0)
 		}
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_admin": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "先驱好像不支持"})
+		return resultFail(map[string]interface{}{"data": "先驱好像不支持"})
 	},
 	"set_group_anonymous": func(bot int64, p gjson.Result) Result {
 		group_id := p.Get("group_id").Int()
 		enable := p.Get("enable").Bool()
 		core.SetAnon(bot, group_id, enable)
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_card": func(bot int64, p gjson.Result) Result {
 		group_id := p.Get("group_id").Int()
 		user_id := p.Get("user_id").Int()
 		card := p.Get("card").Str
 		core.SetGroupCard(bot, group_id, user_id, card)
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_name": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "先驱好像不支持"})
+		return resultFail(map[string]interface{}{"data": "先驱好像不支持"})
 	},
 	"set_group_leave": func(bot int64, p gjson.Result) Result {
 		group_id := p.Get("group_id").Int()
 		core.QuitGroup(bot, group_id)
-		return resultOK(Data{"message_id": 0})
+		return resultOK(map[string]interface{}{"message_id": 0})
 	},
 	"set_group_special_title": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "先驱好像不支持"})
+		return resultFail(map[string]interface{}{"data": "先驱好像不支持"})
 	},
 	"set_friend_add_request": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		flag := p.Get("flag").Str
+		remark := p.Get("remark").Str
+		approve := p.Get("approve").Bool()
+		if approve {
+			core.HandleFriendEvent(bot, core.Str2Int(flag), 10, remark)
+		} else {
+			core.HandleFriendEvent(bot, core.Str2Int(flag), 20, remark)
+		}
+		return resultOK(map[string]interface{}{})
 	},
 	"set_group_add_request": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		flagdata := strings.Split(p.Get("flag").Str, "|")
+		subType := flagdata[0]
+		groupID := flagdata[1]
+		flag := flagdata[2]
+		approve := p.Get("approve").Bool()
+		reason := p.Get("reason").Str
+		if approve {
+			core.HandleGroupEvent(bot, core.Str2Int(subType), 0, core.Str2Int(groupID), core.Str2Int(flag), 10, reason)
+		} else {
+			core.HandleGroupEvent(bot, core.Str2Int(subType), 0, core.Str2Int(groupID), core.Str2Int(flag), 20, reason)
+		}
+		return resultOK(map[string]interface{}{})
 	},
 	"get_login_info": func(bot int64, p gjson.Result) Result {
 		nickname := core.GetNick(bot, bot)
-		return resultOK(Data{"user_id": bot, "nickname": nickname})
+		return resultOK(map[string]interface{}{"user_id": bot, "nickname": nickname})
 	},
 	"get_stranger_info": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
-	// 测试
 	"get_friend_list": func(bot int64, p gjson.Result) Result {
-		list := core.GetFriendList(bot)
-		return resultOK(Data{"data": list})
+		list := core.GetFriendList_B(bot)
+		if list == "" {
+			return resultFail(map[string]interface{}{"data": "ERROR"})
+		}
+		cqFriendList := []CQFriendInfo{}
+		for _, xqFriend := range strings.Split(list, "/n") {
+			cqFriendInfo := CQFriendInfo{
+				UserID:   core.Str2Int(xqFriend),
+				Nickname: "unknow",
+				Remark:   "unknow",
+			}
+			cqFriendList = append(cqFriendList, cqFriendInfo)
+		}
+		return resultOK(cqFriendList)
 	},
 	"get_group_info": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_group_list": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		list := core.GetGroupList_B(bot)
+		if list == "" {
+			return resultFail(map[string]interface{}{"data": "ERROR"})
+		}
+		cqGroupList := []CQGroupInfo{}
+		for _, xqGroup := range strings.Split(list, "/n") {
+			cqGroupInfo := CQGroupInfo{
+				GroupID:        core.Str2Int(xqGroup),
+				GroupName:      "unknow",
+				MemberCount:    0,
+				MaxMemberCount: 0,
+			}
+			cqGroupList = append(cqGroupList, cqGroupInfo)
+		}
+		return resultOK(cqGroupList)
 	},
 	"get_group_member_info": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
+	},
+	"get_group_member_list": func(bot int64, p gjson.Result) Result {
+		group_id := p.Get("group_id").Int()
+		list := core.GetGroupMemberList_C(bot, group_id)
+		if list == "" {
+			return resultFail(map[string]interface{}{"data": "ERROR"})
+		}
+		xqGroupMemberList := XQGroupMemberList{}
+		cqGroupMemberList := []CQGroupMember{}
+		json.Unmarshal([]byte(list), &xqGroupMemberList)
+		for _, xqMember := range xqGroupMemberList.List {
+			cqMember := CQGroupMember{
+				GroupID:         group_id,
+				UserID:          xqMember.QQ,
+				Nickname:        "unknow",
+				Card:            "unknow",
+				Sex:             "unknow",
+				Age:             0,
+				Area:            "unknow",
+				JoinTime:        0,
+				LastSentTime:    0,
+				Level:           core.Int2Str(xqMember.Lv),
+				Role:            "unknow",
+				Unfriendly:      false,
+				Title:           "unknow",
+				TitleExpireTime: 0,
+				CardChangeable:  true,
+			}
+			cqGroupMemberList = append(cqGroupMemberList, cqMember)
+		}
+		return resultOK(cqGroupMemberList)
 	},
 	"get_group_honor_info": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_cookies": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_csrf_token": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_record": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"get_image": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"can_send_image": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"yes": true})
+		return resultFail(map[string]interface{}{"yes": true})
 	},
 	"can_send_record": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"yes": true})
+		return resultFail(map[string]interface{}{"yes": true})
 	},
 	"get_status": func(bot int64, p gjson.Result) Result {
 		online := core.IsOnline(bot, bot)
-		return resultFail(Data{"online": online, "good": true})
+		return resultFail(map[string]interface{}{"online": online, "good": true})
 	},
 	"get_version_info": func(bot int64, p gjson.Result) Result {
 		app_info := gjson.Parse(AppInfoJson)
 		app_version := app_info.Get("pver")
-		return resultFail(Data{"app_name": "OneBot-YaYa", "app_version": app_version, "protocol_version": "v11"})
+		return resultFail(map[string]interface{}{"app_name": "OneBot-YaYa", "app_version": app_version, "protocol_version": "v11"})
 	},
 	"set_restart": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	"clean_cache": func(bot int64, p gjson.Result) Result {
-		return resultFail(Data{"data": "还没写，催更去GitHub提issue"})
+		return resultFail(map[string]interface{}{"data": "还没写，催更去GitHub提issue"})
 	},
 	// 先驱新增
 	"out_put_log": func(bot int64, p gjson.Result) Result {
 		text := p.Get("text").Str
 		core.OutPutLog(text)
-		return resultOK(Data{})
+		return resultOK(map[string]interface{}{})
 	},
 }
 
-func resultOK(data map[string]interface{}) Result {
+func resultOK(data interface{}) Result {
 	return Result{
 		Status:  "ok",
 		Retcode: 200,
 		Data:    data,
-		Echo: Echo{
-			Seq: 0,
-		},
+		Echo:    nil,
 	}
 }
 
-func resultFail(data map[string]interface{}) Result {
+func resultFail(data interface{}) Result {
 	return Result{
 		Status:  "failed",
 		Retcode: 100,
-		Data:    Data{},
-		Echo: Echo{
-			Seq: 0,
-		},
+		Data:    data,
+		Echo:    nil,
 	}
 }
 
@@ -326,9 +439,9 @@ func SendMessage(selfID int64, messageType int64, groupID int64, userID int64, m
 				title := message.Get("data.title").Str
 				content := message.Get("data.content").Str
 				image := message.Get("data.image").Str
-				json := SendCustomMusic(url, audio, title, content, image)
-				TEST("json格式为%v", json)
-				core.SendJSON(selfID, 1, 2, groupID, userID, json)
+				music := SendCustomMusic(url, audio, title, content, image)
+				TEST("json格式为%v", music)
+				core.SendXML(selfID, 1, messageType, groupID, userID, music, 0)
 			} else {
 				out += "[no such element]"
 			}
@@ -355,7 +468,7 @@ func SendMessage(selfID int64, messageType int64, groupID int64, userID int64, m
 	if out != "" {
 		messageID = core.SendMsgEX_V2(selfID, messageType, groupID, userID, out, 0, false, " ")
 	}
-	return resultOK(Data{"message_id": messageID})
+	return resultOK(map[string]interface{}{"message_id": messageID})
 }
 
 func Base64SaveImage(res string) string {
@@ -419,30 +532,13 @@ func Base64SaveVideo(res string) string {
 }
 
 func SendCustomMusic(url string, audio string, title string, content string, image string) string {
-	music := map[string]interface{}{
-		"app":    "com.tencent.structmsg",
-		"desc":   "音乐",
-		"view":   "music",
-		"ver":    "0.0.0.1",
-		"prompt": "[分享]" + title,
-		"meta": map[string]interface{}{
-			"music": map[string]interface{}{
-				"action":           "",
-				"android_pkg_name": "",
-				"app_type":         1,
-				"appid":            100495085,
-				"desc":             content,
-				"jumpUrl":          url,
-				"musicUrl":         audio,
-				"preview":          image,
-				"sourceMsgId":      "0",
-				"source_icon":      "",
-				"source_url":       "",
-				"tag":              "OneBot",
-				"title":            title,
-			},
-		},
-	}
-	data, _ := json.Marshal(music)
-	return string(data)
+	music := fmt.Sprintf(`<?xml version='1.0' encoding='UTF-8' standalone='yes' ?><msg serviceID="2" templateID="1" action="web" brief="[分享] %s" sourceMsgId="0" url="%s" flag="0" adverSign="0" multiMsgFlag="0"><item layout="2"><audio cover="%s" src="%s"/><title>%s</title><summary>%s</summary></item><source name="音乐" icon="https://i.gtimg.cn/open/app_icon/01/07/98/56/1101079856_100_m.png" url="http://web.p.qq.com/qqmpmobile/aio/app.html?id=1101079856" action="app" a_actionData="com.tencent.qqmusic" i_actionData="tencent1101079856://" appid="1101079856" /></msg>`,
+		XmlEscape(title), url, image, audio, XmlEscape(title), XmlEscape(content))
+	return string(music)
+}
+
+func XmlEscape(c string) string {
+	buf := new(bytes.Buffer)
+	_ = xml.EscapeText(buf, []byte(c))
+	return buf.String()
 }
