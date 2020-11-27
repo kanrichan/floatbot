97bafb623be2447ee324f43e00694fd8c7e61395
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-11-27 21:59:21 +0800

    Update onebot.go

--- cqhttp/onebot.go
+++ cqhttp/onebot.go
@@ -15,12 +15,15 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// runOnebot run all server in config
 func (conf *Yaml) runOnebot() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[OneBot] runOnebot()发生不可自动修复错误，无法启动OneBot，请到GitHub提交issue %v", err)
+			ERROR("[OneBot] OneBot =X=> =X=> Start Error: %v", err)
+			WARN("[OneBot] OneBot ==> ==> Sleep")
 		}
 	}()
+	go Conf.heartBeat()
 	for i, _ := range conf.BotConfs {
 		for j, _ := range conf.BotConfs[i].WSSConf {
 			if conf.BotConfs[i].WSSConf[j].Status == 0 && conf.BotConfs[i].WSSConf[j].Enable == true && conf.BotConfs[i].WSSConf[j].Host != "" {
@@ -31,43 +34,51 @@ func (conf *Yaml) runOnebot() {
 		}
 		for k, _ := range conf.BotConfs[i].WSCConf {
 			if conf.BotConfs[i].WSCConf[k].Status == 0 && conf.BotConfs[i].WSCConf[k].Enable == true && conf.BotConfs[i].WSCConf[k].Url != "" {
-				go conf.BotConfs[i].WSCConf[k].WSCListen()
-				go conf.BotConfs[i].WSCConf[k].WSCSend()
+				go conf.BotConfs[i].WSCConf[k].listen()
+				go conf.BotConfs[i].WSCConf[k].send()
 			}
 		}
 		for l, _ := range conf.BotConfs[i].HTTPConf {
 			if conf.BotConfs[i].HTTPConf[l].Status == 0 && conf.BotConfs[i].HTTPConf[l].Enable == true && conf.BotConfs[i].HTTPConf[l].Host != "" {
-				//go conf.BotConfs[i].HTTPConf[l].listen()
+				go conf.BotConfs[i].HTTPConf[l].listen()
 				go conf.BotConfs[i].HTTPConf[l].send()
 			}
 		}
 	}
 }
 
+// heartBeat HeartBeat --> ALL PLUGINS
 func (conf *Yaml) heartBeat() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[OneBot] heartBeat()发生不可自动修复错误，心跳停止，请到GitHub提交issue %v", err)
+			ERROR("[心跳] XQ =X=> =X=> Plugins Error: %v", err)
 		}
 	}()
-	DEBUG("[心跳服务] 开始对已连接端发送心跳...... ")
+	if conf.HeratBeatConf.Interval == 0 || !conf.HeratBeatConf.Enable {
+		return
+	}
+	if conf.HeratBeatConf.Interval < 1000 {
+		INFO("[心跳] Interval %v -> 1000", conf.HeratBeatConf.Interval)
+		conf.HeratBeatConf.Interval = 1000
+	}
+	INFO("[心跳] XQ ==> ==> Plugins")
 	for {
 		time.Sleep(time.Millisecond * time.Duration(conf.HeratBeatConf.Interval))
 		if conf.HeratBeatConf.Enable && conf.HeratBeatConf.Interval != 0 {
 			for i, _ := range conf.BotConfs {
 				for j, _ := range conf.BotConfs[i].WSSConf {
-					if conf.BotConfs[i].WSSConf[j].Status == 1 {
-						conf.BotConfs[i].WSSConf[j].Event <- heartEvent(conf.HeratBeatConf.Interval, conf.BotConfs[i].Bot)
+					if conf.BotConfs[i].WSSConf[j].Status == 1 && conf.BotConfs[i].WSSConf[j].Enable {
+						conf.BotConfs[i].WSSConf[j].Heart <- heartEvent(conf.HeratBeatConf.Interval, conf.BotConfs[i].Bot)
 					}
 				}
 				for k, _ := range conf.BotConfs[i].WSCConf {
-					if conf.BotConfs[i].WSCConf[k].Status == 1 {
-						conf.BotConfs[i].WSCConf[k].Event <- heartEvent(conf.HeratBeatConf.Interval, conf.BotConfs[i].Bot)
+					if conf.BotConfs[i].WSCConf[k].Status == 1 && conf.BotConfs[i].WSCConf[k].Enable {
+						conf.BotConfs[i].WSCConf[k].Heart <- heartEvent(conf.HeratBeatConf.Interval, conf.BotConfs[i].Bot)
 					}
 				}
 				for l, _ := range conf.BotConfs[i].HTTPConf {
-					if conf.BotConfs[i].HTTPConf[l].Status == 1 {
-						conf.BotConfs[i].HTTPConf[l].Event <- heartEvent(conf.HeratBeatConf.Interval, conf.BotConfs[i].Bot)
+					if conf.BotConfs[i].HTTPConf[l].Status == 1 && conf.BotConfs[i].HTTPConf[l].Enable {
+						conf.BotConfs[i].HTTPConf[l].Heart <- heartEvent(conf.HeratBeatConf.Interval, conf.BotConfs[i].Bot)
 					}
 				}
 			}
@@ -88,25 +99,13 @@ func heartEvent(interval int64, bot int64) []byte {
 	return event
 }
 
-func (c *WSCYaml) WSCConnect() {
-	for {
-		conn, _, err := websocket.DefaultDialer.Dial(c.Url, c.WSCHeader())
-		if err != nil {
-			DEBUG("[反向WS] Bot %v 与 %v 服务器连接出现错误: %v ", c.BotID, c.Url, err)
-			time.Sleep(time.Millisecond * time.Duration(c.ReconnectInterval))
-			continue
-		} else {
-			c.Conn = conn
-			c.Status = 1
-			c.WSCHandShake()
-			INFO("[反向WS] Bot %v 与 %v 服务器连接成功", c.BotID, c.Url)
-			break
-		}
+// 反向WS
+
+func (c *WSCYaml) connect() {
+	if c.ReconnectInterval < 1000 {
+		INFO("[连接][反向WS][%v] ReconnectInterval %v -> 1000", c.BotID, c.ReconnectInterval)
+		c.ReconnectInterval = 1000
 	}
-
-}
-
-func (c *WSCYaml) WSCHeader() http.Header {
 	header := http.Header{
 		"X-Client-Role": []string{"Universal"},
 		"X-Self-ID":     []string{strconv.FormatInt(c.BotID, 10)},
@@ -115,10 +114,24 @@ func (c *WSCYaml) WSCHeader() http.Header {
 	if c.AccessToken != "" {
 		header["Authorization"] = []string{"Token " + c.AccessToken}
 	}
-	return header
+	for {
+		conn, _, err := websocket.DefaultDialer.Dial(c.Url, header)
+		if err != nil {
+			DEBUG("[连接][反向WS][%v] BOT =X=> ==> %v ", c.BotID, c.Url)
+			time.Sleep(time.Millisecond * time.Duration(c.ReconnectInterval))
+			continue
+		} else {
+			c.Conn = conn
+			c.Status = 1
+			c.handShake()
+			INFO("[连接][反向WS][%v] BOT ==> ==> %v ", c.BotID, c.Url)
+			break
+		}
+	}
+
 }
 
-func (c *WSCYaml) WSCHandShake() {
+func (c *WSCYaml) handShake() {
 	handshake := map[string]string{
 		"meta_event_type": "lifecycle",
 		"post_type":       "meta_event",
@@ -128,19 +141,19 @@ func (c *WSCYaml) WSCHandShake() {
 	}
 	event, _ := json.Marshal(handshake)
 	if c.Status == 1 {
-		c.Event <- event
+		c.Heart <- event
 	}
 }
 
-func (c *WSCYaml) WSCListen() {
+func (c *WSCYaml) listen() {
 	defer func() {
 		if err := recover(); err != nil {
 			c.Status = 0
-			ERROR("[监听服务] Bot %v 监听 %v 服务发生错误，正在自动恢复中...... %v，", c.BotID, c.Url, err)
-			c.WSCListen()
+			WARN("[监听][反向WS][%v] BOT =X=> ==> %v ERROR: %v", c.BotID, c.Url, err)
+			c.listen()
 		}
 	}()
-	c.WSCConnect()
+	c.connect()
 	// 等待wsc连接成功
 	for {
 		if c.Status == 1 {
@@ -148,7 +161,7 @@ func (c *WSCYaml) WSCListen() {
 		}
 	}
 	// 监听wsc
-	DEBUG("[监听服务] Bot %v 开始监听 %v ...... ", c.BotID, c.Url)
+	INFO("[监听][反向WS][%v] BOT ==> ==> %v ", c.BotID, c.Url)
 	for {
 		_, buf, err := c.Conn.ReadMessage()
 		if err != nil {
@@ -158,11 +171,11 @@ func (c *WSCYaml) WSCListen() {
 	}
 }
 
-func (c *WSCYaml) WSCSend() {
+func (c *WSCYaml) send() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[上报服务] Bot %v 向 %v 上报发生错误，正在自动恢复中...... %v，", c.BotID, c.Url, err)
-			c.WSCSend()
+			WARN("[上报][反向WS][%v] BOT =X=> ==> %v ERROR: %v", c.BotID, c.Url, err)
+			c.send()
 		}
 	}()
 	// 等待wsc连接成功
@@ -171,7 +184,7 @@ func (c *WSCYaml) WSCSend() {
 			break
 		}
 	}
-	DEBUG("[上报服务] Bot %v 向 %v 开始推送...... ", c.BotID, c.Url)
+	INFO("[上报][反向WS][%v] BOT ==> ==> %v ", c.BotID, c.Url)
 	for {
 		select {
 		case send := <-c.Event:
@@ -180,11 +193,21 @@ func (c *WSCYaml) WSCSend() {
 			if err != nil {
 				panic(err)
 			} else {
-				DEBUG("[上报服务] Bot %v 向 %v 上报：%v", c.BotID, c.Url, string(send))
+				DEBUG("[上报][反向WS][%v] %v <- %v", c.BotID, c.Url, string(send))
+			}
+		case send := <-c.Heart:
+			_ = c.Conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
+			err := c.Conn.WriteMessage(websocket.TextMessage, send)
+			if err != nil {
+				panic(err)
+			} else {
+				META("[心跳][反向WS][%v] %v <- %v", c.BotID, c.Url, string(send))
 			}
 		}
 	}
 }
+
+// 正向WS
 
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
 
@@ -195,7 +218,7 @@ func (s *WSSYaml) start() {
 func (s *WSSYaml) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[正向WS] Bot %v 在 %v:%v 升级发生错误 %v，", s.BotID, s.Host, s.Port, err)
+			ERROR("[连接][正向WS][%v] BOT =X=> ==> %v:%v Error: %v", s.BotID, s.Host, s.Port, err)
 		}
 	}()
 	if r.Header.Get("Authorization") == "Token "+s.AccessToken || s.AccessToken == "" {
@@ -207,7 +230,7 @@ func (s *WSSYaml) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.Status = 1
 		s.handShake()
 	} else {
-		WARN("[正向WS] Bot %v 拒绝与 %v:%v 连接 token error", s.BotID, s.Host, s.Port)
+		ERROR("[连接][正向WS][%v] BOT X Token X %v:%v", s.BotID, s.Host, s.Port)
 	}
 }
 
@@ -228,7 +251,7 @@ func (s *WSSYaml) handShake() {
 func (s *WSSYaml) listen() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[监听服务] Bot %v 在 %v:%v 监听发生错误 %v，", s.BotID, s.Host, s.Port, err)
+			ERROR("[监听][正向WS][%v] BOT =X=> ==> %v:%v Error: %v", s.BotID, s.Host, s.Port, err)
 			s.listen()
 		}
 	}()
@@ -237,7 +260,7 @@ func (s *WSSYaml) listen() {
 			break
 		}
 	}
-	DEBUG("[监听服务] Bot %v 在 %v:%v 开始监听...... ", s.BotID, s.Host, s.Port)
+	INFO("[监听][正向WS][%v] BOT ==> ==> %v:%v", s.BotID, s.Host, s.Port)
 	for {
 		for i, conn := range s.Conn {
 			_, buf, err := conn.ReadMessage()
@@ -256,7 +279,7 @@ func (s *WSSYaml) listen() {
 func (s *WSSYaml) send() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[上报服务] Bot %v 在 %v:%v 上报发生错误 %v，", s.BotID, s.Host, s.Port, err)
+			ERROR("[上报][正向WS][%v] BOT =X=> ==> %v:%v Error: %v", s.BotID, s.Host, s.Port, err)
 			s.send()
 		}
 	}()
@@ -265,7 +288,7 @@ func (s *WSSYaml) send() {
 			break
 		}
 	}
-	DEBUG("[上报服务] Bot %v 在 %v:%v 开始推送...... ", s.BotID, s.Host, s.Port)
+	INFO("[上报][正向WS][%v] BOT ==> ==> %v:%v", s.BotID, s.Host, s.Port)
 	for {
 		select {
 		case send := <-s.Event:
@@ -279,7 +302,21 @@ func (s *WSSYaml) send() {
 					}
 					panic(err)
 				} else {
-					DEBUG("[上报服务] Bot %v 在 %v:%v 上报：%v", s.BotID, s.Host, s.Port, string(send))
+					DEBUG("[上报][正向WS][%v] %v:%v <- %v", s.BotID, s.Host, s.Port, string(send))
+				}
+			}
+		case send := <-s.Heart:
+			for i, conn := range s.Conn {
+				_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
+				err := conn.WriteMessage(websocket.TextMessage, send)
+				if err != nil {
+					s.Conn = append(s.Conn[:i], s.Conn[i+1:]...)
+					if len(s.Conn) < 1 {
+						s.Status = 0
+					}
+					panic(err)
+				} else {
+					META("[心跳][正向WS][%v] %v:%v <- %v", s.BotID, s.Host, s.Port, string(send))
 				}
 			}
 		}
@@ -289,18 +326,18 @@ func (s *WSSYaml) send() {
 func (h *HTTPYaml) listen() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[监听服务] Bot %v 在 %v:%v 监听发生错误 %v，", h.BotID, h.Host, h.Port, err)
+			ERROR("[连接][HTTP][%v] BOT =X=> ==> %v:%v Error: %v", h.BotID, h.Host, h.Port, err)
 			h.listen()
 		}
 	}()
-	DEBUG("[监听服务] Bot %v 在 %v:%v 开始监听...... ", h.BotID, h.Host, h.Port)
+	INFO("[连接][HTTP][%v] BOT ==> ==> %v:%v", h.BotID, h.Host, h.Port)
 	http.ListenAndServe(fmt.Sprintf("%v:%v", h.Host, h.Port), h)
 }
 
 func (h *HTTPYaml) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[HTTP] Bot %v 在 %v:%v 监听发生错误 %v，", h.BotID, h.Host, h.Port, err)
+			ERROR("[监听][HTTP][%v] BOT =X=> ==> %v:%v Error: %v", h.BotID, h.Host, h.Port, err)
 		}
 	}()
 
@@ -308,16 +345,16 @@ func (h *HTTPYaml) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		h.Status = 1
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(r.Body)
-		w.Write(h.wscApi(r.URL.Path, buf.Bytes()))
+		w.Write(h.apiReply(r.URL.Path, buf.Bytes()))
 	} else {
-		WARN("[正向WS] Bot %v 拒绝与 %v:%v 连接 secret error", h.BotID, h.Host, h.Port)
+		WARN("[监听][HTTP][%v] BOT X Secret X %v:%v", h.BotID, h.Host, h.Port)
 	}
 }
 
 func (h *HTTPYaml) send() {
 	defer func() {
 		if err := recover(); err != nil {
-			ERROR("[上报服务] Bot %v 在 %v 上报发生错误 %v，", h.BotID, h.PostUrl, err)
+			ERROR("[上报][HTTP][%v] BOT =X=> ==> %v Error: %v", h.BotID, h.PostUrl, err)
 			h.Status = 0
 			h.send()
 		}
@@ -328,30 +365,50 @@ func (h *HTTPYaml) send() {
 		},
 	}
 	h.Status = 1
-	DEBUG("[上报服务] Bot %v 在 %v 开始推送...... ", h.BotID, h.PostUrl)
+	INFO("[上报][HTTP][%v] BOT ==> ==> %v", h.BotID, h.PostUrl)
 	for {
 		select {
 		case send := <-h.Event:
-			req, _ := http.NewRequest("POST", h.PostUrl, bytes.NewBuffer(send))
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("X-Self-ID", strconv.FormatInt(h.BotID, 10))
-			req.Header.Set("User-Agent", "CQHttp/4.15.0")
-			if h.Secret != "" {
-				mac := hmac.New(sha1.New, []byte(h.Secret))
-				mac.Write(send)
-				req.Header.Set("X-Signature", "sha1="+hex.EncodeToString(mac.Sum(nil)))
-			}
-			resp, err := client.Do(req)
-			if err != nil {
-				ERROR("[HTTP POST] Bot %v 在 %v 上报出错", h.BotID, h.PostUrl)
-			} else {
-				DEBUG("[HTTP POST] Bot %v 在 %v 上报：%v", h.BotID, h.PostUrl, string(send))
-				body, _ := ioutil.ReadAll(resp.Body)
-				if string(body) != "" {
-					h.reply(send, body)
+			if h.PostUrl != "" {
+				req, _ := http.NewRequest("POST", h.PostUrl, bytes.NewBuffer(send))
+				req.Header.Set("Content-Type", "application/json")
+				req.Header.Set("X-Self-ID", strconv.FormatInt(h.BotID, 10))
+				req.Header.Set("User-Agent", "CQHttp/4.15.0")
+				if h.Secret != "" {
+					mac := hmac.New(sha1.New, []byte(h.Secret))
+					mac.Write(send)
+					req.Header.Set("X-Signature", "sha1="+hex.EncodeToString(mac.Sum(nil)))
 				}
+				resp, err := client.Do(req)
+				if err != nil {
+					panic(err)
+				} else {
+					DEBUG("[上报][HTTP][%v] %v <- %v", h.BotID, h.PostUrl, string(send))
+					body, _ := ioutil.ReadAll(resp.Body)
+					if string(body) != "" {
+						h.reply(send, body)
+					}
+				}
+				resp.Body.Close()
 			}
-			resp.Body.Close()
+		case send := <-h.Heart:
+			if h.PostUrl != "" {
+				req, _ := http.NewRequest("POST", h.PostUrl, bytes.NewBuffer(send))
+				req.Header.Set("Content-Type", "application/json")
+				req.Header.Set("X-Self-ID", strconv.FormatInt(h.BotID, 10))
+				req.Header.Set("User-Agent", "CQHttp/4.15.0")
+				if h.Secret != "" {
+					mac := hmac.New(sha1.New, []byte(h.Secret))
+					mac.Write(send)
+					req.Header.Set("X-Signature", "sha1="+hex.EncodeToString(mac.Sum(nil)))
+				}
+				resp, err := client.Do(req)
+				if err != nil {
+					panic(err)
+				}
+				META("[心跳][HTTP][%v] %v <- %v", h.BotID, h.PostUrl, string(send))
+				resp.Body.Close()
+			}
 		}
 	}
 }
