838fa6732245fd604b1061ca81ac5e799009582d
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-12-01 00:28:30 +0800

    Update cqcode

--- cqhttp/cqcode.go
+++ cqhttp/cqcode.go
@@ -63,25 +63,39 @@ func cqCode2Array(text gjson.Result) gjson.Result {
 
 				switch codeType {
 				case "face":
-					faceR := regexp.MustCompile(`id=(.*)`)
-					id := faceR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "face", "data": map[string]interface{}{"id": id}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "face", "data": map[string]interface{}{
+						"id": cqCodeParm(codeParm, "id"),
+					}})
 				case "image":
-					imageR := regexp.MustCompile(`file=(.*)`)
-					file := imageR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "image", "data": map[string]interface{}{"file": file}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "image", "data": map[string]interface{}{
+						"file":    cqCodeParm(codeParm, "file"),
+						"type":    cqCodeParm(codeParm, "type"),
+						"url":     cqCodeParm(codeParm, "url"),
+						"cache":   cqCodeParm(codeParm, "cache"),
+						"proxy":   cqCodeParm(codeParm, "proxy"),
+						"timeout": cqCodeParm(codeParm, "timeout"),
+					}})
 				case "record":
-					recordR := regexp.MustCompile(`file=(.*)`)
-					file := recordR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "record", "data": map[string]interface{}{"file": file}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "record", "data": map[string]interface{}{
+						"file":    cqCodeParm(codeParm, "file"),
+						"magic":   cqCodeParm(codeParm, "magic"),
+						"url":     cqCodeParm(codeParm, "url"),
+						"cache":   cqCodeParm(codeParm, "cache"),
+						"proxy":   cqCodeParm(codeParm, "proxy"),
+						"timeout": cqCodeParm(codeParm, "timeout"),
+					}})
 				case "video":
-					videoR := regexp.MustCompile(`file=(.*)`)
-					file := videoR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "viedo", "data": map[string]interface{}{"file": file}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "video", "data": map[string]interface{}{
+						"file":    cqCodeParm(codeParm, "file"),
+						"url":     cqCodeParm(codeParm, "url"),
+						"cache":   cqCodeParm(codeParm, "cache"),
+						"proxy":   cqCodeParm(codeParm, "proxy"),
+						"timeout": cqCodeParm(codeParm, "timeout"),
+					}})
 				case "at":
-					atR := regexp.MustCompile(`qq=(.*)`)
-					qq := atR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "at", "data": map[string]interface{}{"qq": qq}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "at", "data": map[string]interface{}{
+						"qq": cqCodeParm(codeParm, "qq"),
+					}})
 				case "rps":
 					paramsArray = append(paramsArray, map[string]interface{}{"type": "rps", "data": map[string]interface{}{}})
 				case "dice":
@@ -89,105 +103,79 @@ func cqCode2Array(text gjson.Result) gjson.Result {
 				case "shake":
 					paramsArray = append(paramsArray, map[string]interface{}{"type": "shake", "data": map[string]interface{}{}})
 				case "poke":
-					pokeR := regexp.MustCompile(`type=(.*?),id=(.*)`)
-					typ := pokeR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					id := pokeR.FindAllStringSubmatch(codeParm, -1)[0][2]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "poke", "data": map[string]interface{}{"type": typ, "id": id}})
+					DEBUG("[CQ码解析] %v 不支持，将以原样发送", code[0][0])
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "text", "data": map[string]interface{}{
+						"text": code[0][0],
+					}})
 				case "anonymous":
 					paramsArray = append(paramsArray, map[string]interface{}{"type": "anonymous", "data": map[string]interface{}{}})
 				case "share":
-					shareR1 := regexp.MustCompile(`url=(.*?),title=(.*?),content=(.*?),image=(.*)`)
-					shareR2 := regexp.MustCompile(`url=(.*?),title=(.*?),content=(.*)`)
-					shareR3 := regexp.MustCompile(`url=(.*?),title=(.*?),image=(.*)`)
-					shareR4 := regexp.MustCompile(`url=(.*?),title=(.*)`)
-					if len(shareR1.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						url := shareR1.FindAllStringSubmatch(codeParm, -1)[0][1]
-						title := shareR1.FindAllStringSubmatch(codeParm, -1)[0][2]
-						content := shareR1.FindAllStringSubmatch(codeParm, -1)[0][3]
-						image := shareR1.FindAllStringSubmatch(codeParm, -1)[0][4]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "share", "data": map[string]interface{}{"url": url, "title": title, "content": content, "image": image}})
-					} else if len(shareR2.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						url := shareR2.FindAllStringSubmatch(codeParm, -1)[0][1]
-						title := shareR2.FindAllStringSubmatch(codeParm, -1)[0][2]
-						content := shareR2.FindAllStringSubmatch(codeParm, -1)[0][3]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "share", "data": map[string]interface{}{"url": url, "title": title, "content": content, "image": ""}})
-					} else if len(shareR3.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						url := shareR3.FindAllStringSubmatch(codeParm, -1)[0][1]
-						title := shareR3.FindAllStringSubmatch(codeParm, -1)[0][2]
-						image := shareR3.FindAllStringSubmatch(codeParm, -1)[0][3]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "share", "data": map[string]interface{}{"url": url, "title": title, "content": "", "image": image}})
-					} else if len(shareR4.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						url := shareR4.FindAllStringSubmatch(codeParm, -1)[0][1]
-						title := shareR4.FindAllStringSubmatch(codeParm, -1)[0][2]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "share", "data": map[string]interface{}{"url": url, "title": title, "content": "", "image": ""}})
-					}
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "share", "data": map[string]interface{}{
+						"url":     cqCodeParm(codeParm, "url"),
+						"title":   cqCodeParm(codeParm, "title"),
+						"content": cqCodeParm(codeParm, "content"),
+						"image":   cqCodeParm(codeParm, "image"),
+					}})
 				case "contact":
-					contactR := regexp.MustCompile(`type=(.*?),id=(.*)`)
-					typ := contactR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					id := contactR.FindAllStringSubmatch(codeParm, -1)[0][2]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "contact", "data": map[string]interface{}{"type": typ, "id": id}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "contact", "data": map[string]interface{}{
+						"type": cqCodeParm(codeParm, "type"),
+						"id":   cqCodeParm(codeParm, "id"),
+					}})
 				case "location":
-					locationR1 := regexp.MustCompile(`lat=(.*?),lon=(.*?),title=(.*?),content=(.*)`)
-					locationR2 := regexp.MustCompile(`lat=(.*?),lon=(.*?),title=(.*)`)
-					locationR3 := regexp.MustCompile(`lat=(.*?),lon=(.*?),content=(.*)`)
-					locationR4 := regexp.MustCompile(`lat=(.*?),lon=(.*)`)
-					if len(locationR1.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						lat := locationR1.FindAllStringSubmatch(codeParm, -1)[0][1]
-						lon := locationR1.FindAllStringSubmatch(codeParm, -1)[0][2]
-						title := locationR1.FindAllStringSubmatch(codeParm, -1)[0][3]
-						content := locationR1.FindAllStringSubmatch(codeParm, -1)[0][4]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "location", "data": map[string]interface{}{"lat": lat, "lon": lon, "title": title, "content": content}})
-					} else if len(locationR2.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						lat := locationR2.FindAllStringSubmatch(codeParm, -1)[0][1]
-						lon := locationR2.FindAllStringSubmatch(codeParm, -1)[0][2]
-						title := locationR2.FindAllStringSubmatch(codeParm, -1)[0][3]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "location", "data": map[string]interface{}{"lat": lat, "lon": lon, "title": title, "content": ""}})
-					} else if len(locationR3.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						lat := locationR3.FindAllStringSubmatch(codeParm, -1)[0][1]
-						lon := locationR3.FindAllStringSubmatch(codeParm, -1)[0][2]
-						content := locationR3.FindAllStringSubmatch(codeParm, -1)[0][3]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "location", "data": map[string]interface{}{"lat": lat, "lon": lon, "title": "", "content": content}})
-					} else if len(locationR4.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						lat := locationR4.FindAllStringSubmatch(codeParm, -1)[0][1]
-						lon := locationR4.FindAllStringSubmatch(codeParm, -1)[0][2]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "location", "data": map[string]interface{}{"lat": lat, "lon": lon, "title": "", "content": ""}})
-					}
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "location", "data": map[string]interface{}{
+						"lat":     cqCodeParm(codeParm, "lat"),
+						"lon":     cqCodeParm(codeParm, "lon"),
+						"title":   cqCodeParm(codeParm, "title"),
+						"content": cqCodeParm(codeParm, "content"),
+					}})
 				case "music":
-					musicR1 := regexp.MustCompile(`type=(.*?),url=(.*?),audio=(.*?),title=(.*?),content=(.*?),image=(.*)`)
-					musicR2 := regexp.MustCompile(`type=(.*?),id=(.*)`)
-					if len(musicR1.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						typ := musicR1.FindAllStringSubmatch(codeParm, -1)[0][1]
-						url := musicR1.FindAllStringSubmatch(codeParm, -1)[0][2]
-						audio := musicR1.FindAllStringSubmatch(codeParm, -1)[0][3]
-						title := musicR1.FindAllStringSubmatch(codeParm, -1)[0][4]
-						content := musicR1.FindAllStringSubmatch(codeParm, -1)[0][5]
-						image := musicR1.FindAllStringSubmatch(codeParm, -1)[0][6]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "music", "data": map[string]interface{}{"type": typ, "url": url, "audio": audio, "title": title, "content": content, "image": image}})
-					} else if len(musicR2.FindAllStringSubmatch(codeParm, -1)) != 0 {
-						typ := musicR2.FindAllStringSubmatch(codeParm, -1)[0][1]
-						id := musicR2.FindAllStringSubmatch(codeParm, -1)[0][2]
-						paramsArray = append(paramsArray, map[string]interface{}{"type": "music", "data": map[string]interface{}{"type": typ, "id": id}})
+					if cqCodeParm(codeParm, "id") != "" {
+						paramsArray = append(paramsArray, map[string]interface{}{"type": "music", "data": map[string]interface{}{
+							"type": cqCodeParm(codeParm, "type"),
+							"id":   cqCodeParm(codeParm, "id"),
+						}})
+					} else {
+						paramsArray = append(paramsArray, map[string]interface{}{"type": "music", "data": map[string]interface{}{
+							"type":    cqCodeParm(codeParm, "type"),
+							"url":     cqCodeParm(codeParm, "url"),
+							"audio":   cqCodeParm(codeParm, "audio"),
+							"title":   cqCodeParm(codeParm, "title"),
+							"content": cqCodeParm(codeParm, "content"),
+							"image":   cqCodeParm(codeParm, "image"),
+						}})
 					}
 				case "reply":
-					paramsArray = append(paramsArray, map[string]interface{}{"type": text, "data": map[string]interface{}{"text": codeParm}})
+					DEBUG("[CQ码解析] %v 不支持，将以原样发送", code[0][0])
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "text", "data": map[string]interface{}{
+						"text": code[0][0],
+					}})
 				case "forward":
-					paramsArray = append(paramsArray, map[string]interface{}{"type": text, "data": map[string]interface{}{"text": codeParm}})
+					DEBUG("[CQ码解析] %v 不支持，将以原样发送", code[0][0])
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "text", "data": map[string]interface{}{
+						"text": code[0][0],
+					}})
 				case "node":
-					paramsArray = append(paramsArray, map[string]interface{}{"type": text, "data": map[string]interface{}{"text": codeParm}})
+					DEBUG("[CQ码解析] %v 不支持，将以原样发送", code[0][0])
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "text", "data": map[string]interface{}{
+						"text": code[0][0],
+					}})
 				case "xml":
-					xmlR := regexp.MustCompile(`data=(.*)`)
-					data := xmlR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "xml", "data": map[string]interface{}{"data": data}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "xml", "data": map[string]interface{}{
+						"data": cqCodeParm(codeParm, "data"),
+					}})
 				case "json":
-					jsonR := regexp.MustCompile(`data=(.*)`)
-					data := jsonR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "json", "data": map[string]interface{}{"data": data}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "json", "data": map[string]interface{}{
+						"data": cqCodeParm(codeParm, "data"),
+					}})
 				case "emoji":
-					faceR := regexp.MustCompile(`id=(.*)`)
-					id := faceR.FindAllStringSubmatch(codeParm, -1)[0][1]
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "emoji", "data": map[string]interface{}{"id": id}})
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "emoji", "data": map[string]interface{}{
+						"id": cqCodeParm(codeParm, "id"),
+					}})
 				default:
-					paramsArray = append(paramsArray, map[string]interface{}{"type": "error", "data": map[string]interface{}{"error": e}})
+					DEBUG("[CQ码解析] %v 解析失败，将以原样发送", code[0][0])
+					paramsArray = append(paramsArray, map[string]interface{}{"type": "text", "data": map[string]interface{}{
+						"text": code[0][0],
+					}})
 				}
 			}
 		}
@@ -217,7 +205,15 @@ func xq2cqCode(message string) string {
 	for _, p := range pic.FindAllStringSubmatch(message, -1) {
 		oldpic := p[0]
 		md5 := strings.ToUpper(fmt.Sprintf("%s%s%s%s%s", p[1], p[2], p[3], p[4], p[5]))
-		newpic := fmt.Sprintf("[CQ:image,file=%s%s,url=http://gchat.qpic.cn/gchatpic_new//--%s/0]", md5, p[6], md5)
+		newpic := fmt.Sprintf("[CQ:image,file=%s.image,url=http://gchat.qpic.cn/gchatpic_new//--%s/0]", md5, md5)
+		message = strings.ReplaceAll(message, oldpic, newpic)
+	}
+
+	pic2 := regexp.MustCompile(`\[pic={(.*?)-(.*?)-(.*?)-(.*?)-(.*?)}(\..*?)]`)
+	for _, p := range pic2.FindAllStringSubmatch(message, -1) {
+		oldpic := p[0]
+		md5 := strings.ToUpper(fmt.Sprintf("%s%s%s%s%s", p[1], p[2], p[3], p[4], p[5]))
+		newpic := fmt.Sprintf("[CQ:image,file=%s.image,url=http://gchat.qpic.cn/gchatpic_new//--%s/0]", md5, md5)
 		message = strings.ReplaceAll(message, oldpic, newpic)
 	}
 
@@ -263,4 +259,17 @@ func cq2xqCode(message string) string {
 		message = strings.ReplaceAll(message, oldpic, newpic)
 	}
 	return message
+}
+
+func cqCodeParm(codeParm string, field string) string {
+	if !strings.Contains(codeParm, field+"=") {
+		return ""
+	}
+	p := strings.Index(codeParm, field+"=") + len(field) + 1
+	temp := codeParm[p:]
+	s := strings.Index(temp, ",")
+	if s == -1 {
+		return temp
+	}
+	return temp[:s]
 }
