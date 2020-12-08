efd41a4406c1adfb3980f669bf96c4e7eee06790
Yiwen-Chan <yiwen_chan@foxmail.com>
2020-12-08 19:19:07 +0800

    :green_heart: Build pass

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ SET CGO_LDFLAGS=-Wl,--kill-at
 SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=386
-go build -ldflags "-s -w" -buildmode=c-shared -o yaya-cqhttp.XQ.dll
+go build -ldflags "-s -w" -buildmode=c-shared -o OneBot-YaYa.XQ.dll
 pause
