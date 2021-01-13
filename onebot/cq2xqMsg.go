f1af75787e8bc106b98c77dd06cdf2ca3847b30d
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-01-13 13:57:04 +0800

    :sparkles: Add emoji's message escape

--- onebot/cq2xqMsg.go
+++ onebot/cq2xqMsg.go
@@ -1,7 +1,6 @@
 package onebot
 
 import (
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"math/rand"
@@ -192,26 +191,6 @@ func messageSplit(texts string) string {
 
 func (target cq2xqMsgToWhere) cq2xqText(message gjson.Result) string {
 	return emoji2xq(message.Get("data.*").Str)
-}
-
-func emoji2xq(text string) string {
-	data := []byte(text)
-	ret := []byte{}
-	skip := 0
-	for i, _ := range data {
-		if skip > 1 {
-			skip -= 1
-			continue
-		}
-		if data[i] == byte(240) && data[i+1] == byte(159) {
-			code := hex.EncodeToString(data[i : i+4])
-			ret = append(ret, []byte(fmt.Sprintf("[emoji=%s]", code))...)
-			skip = 4
-		} else {
-			ret = append(ret, data[i])
-		}
-	}
-	return string(ret)
 }
 
 func (target cq2xqMsgToWhere) cq2xqFace(message gjson.Result) string {
