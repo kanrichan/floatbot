1de8b5d7a844799845175ae654ba669398b95789
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-02-22 23:55:31 +0800

    🔨 增加websocket_client连接管理

--- server/websocket_client.go
+++ server/websocket_client.go
@@ -9,74 +9,51 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-type WebSocketClient struct {
-	ID                 int64  `yaml:"-"`
-	Name               string `yaml:"name"`
-	Enable             bool   `yaml:"enable"`
-	Url                string `yaml:"url"`
-	ApiUrl             string `yaml:"api_url"`
-	EventUrl           string `yaml:"event_url"`
-	UseUniversalClient bool   `yaml:"use_universal_client"`
-	AccessToken        string `yaml:"access_token"`
-	PostMessageFormat  string `yaml:"post_message_format"`
-	HeartBeatInterval  int64  `yaml:"reconnect_interval"`
-	ReconnectInterval  int64  `yaml:"reconnect_interval"`
-
-	Conn *websocket.Conn `yaml:"-"`
-
-	StopSend      chan bool `yaml:"-"`
-	StopHeartBeat chan bool `yaml:"-"`
-
-	ConnectStatus   string `yaml:"-"`
-	ListenStatus    string `yaml:"-"`
-	SendStatus      string `yaml:"-"`
-	HeartBeatStatus string `yaml:"-"`
-
-	SendChan chan []byte `yaml:"-"`
-}
-
 func (this *WebSocketClient) Connect() {
 	this.ConnectStatus = "connect"
 	defer func() {
-		this.ConnectStatus = "wait"
 		recover()
 	}()
 	for {
-		// 连接
-		header := http.Header{
-			"X-Client-Role": []string{"Universal"},
-			"X-Self-ID":     []string{strconv.FormatInt(this.ID, 10)},
-			"User-Agent":    []string{"CQHttp/4.15.0"},
+		select {
+		case <-time.After(time.Millisecond * time.Duration(this.ReconnectInterval)):
+			// 连接
+			header := http.Header{
+				"X-Client-Role": []string{"Universal"},
+				"X-Self-ID":     []string{strconv.FormatInt(this.ID, 10)},
+				"User-Agent":    []string{"CQHttp/4.15.0"},
+			}
+			conn, _, err := websocket.DefaultDialer.Dial(this.Url, header)
+			if err != nil {
+				continue
+			}
+			// 元事件 表示OneBot启动成功
+			// https://github.com/howmanybots/onebot/blob/master/v11/specs/event/meta.md#%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F
+			handshake, _ := json.Marshal(map[string]string{
+				"meta_event_type": "lifecycle",
+				"post_type":       "meta_event",
+				"self_id":         strconv.FormatInt(this.ID, 10),
+				"sub_type":        "connect",
+				"time":            strconv.FormatInt(time.Now().Unix(), 10),
+			})
+			if err := conn.WriteMessage(websocket.TextMessage, handshake); err != nil {
+				continue
+			}
+			_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
+			this.Conn = conn
+			this.ConnectStatus = "ok"
+			return
+		case <-this.StopConnect:
+			this.ConnectStatus = "wait"
+			return
 		}
-		conn, _, err := websocket.DefaultDialer.Dial(this.Url, header)
-		if err != nil {
-			time.Sleep(time.Millisecond * time.Duration(this.ReconnectInterval))
-			continue
-		}
-		// 元事件 表示OneBot启动成功
-		// https://github.com/howmanybots/onebot/blob/master/v11/specs/event/meta.md#%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F
-		handshake, _ := json.Marshal(map[string]string{
-			"meta_event_type": "lifecycle",
-			"post_type":       "meta_event",
-			"self_id":         strconv.FormatInt(this.ID, 10),
-			"sub_type":        "connect",
-			"time":            strconv.FormatInt(time.Now().Unix(), 10),
-		})
-		if err := this.Conn.WriteMessage(websocket.TextMessage, handshake); err != nil {
-			time.Sleep(time.Millisecond * time.Duration(this.ReconnectInterval))
-			continue
-		}
-		_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
-		this.Conn = conn
-		break
 	}
-	this.ConnectStatus = "ok"
 }
 
 func (this *WebSocketClient) Listen() {
 	this.ListenStatus = "ok"
 	defer func() {
-		this.ListenStatus = "wait"
+		this.ListenStatus = "error"
 		recover()
 	}()
 	// TODO 监听wsc
@@ -85,7 +62,7 @@ func (this *WebSocketClient) Listen() {
 		if err != nil {
 			panic(err)
 		}
-		ret := CallApi("WSC", buf)
+		ret := WebsocketClientCall(buf)
 		this.SendChan <- ret
 	}
 }
@@ -94,26 +71,19 @@ func (this *WebSocketClient) Send() {
 	this.SendStatus = "ok"
 	defer func() {
 		this.SendStatus = "wait"
-		recover()
+		if err := recover(); err != nil {
+			this.SendStatus = "error"
+		}
 	}()
 	for {
 		select {
+		case <-this.StopSend:
+			return
 		case send := <-this.SendChan:
 			if err := this.Conn.WriteMessage(websocket.TextMessage, send); err != nil {
 				panic(err)
 			}
-		case <-this.StopHeartBeat:
-			this.HeartBeatStatus = "wait"
-			return
-		}
-	}
-}
-
-func (this *WebSocketClient) HeartBeat() {
-	this.HeartBeatStatus = "ok"
-	for {
-		select {
-		case <-time.After(time.Second * time.Duration(this.HeartBeatInterval)):
+		case <-time.After(time.Millisecond * time.Duration(this.HeartBeatInterval)):
 			// TODO OneBot标准 元事件 心跳
 			// https://github.com/howmanybots/onebot/blob/master/v11/specs/event/meta.md#%E5%BF%83%E8%B7%B3
 			heartbeat, _ := json.Marshal(
@@ -130,9 +100,6 @@ func (this *WebSocketClient) HeartBeat() {
 				},
 			)
 			this.SendChan <- heartbeat
-		case <-this.StopHeartBeat:
-			this.HeartBeatStatus = "wait"
-			return
 		}
 	}
 }
