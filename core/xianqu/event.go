e6867eb132fb56e6f44cbe0a8f1374e87c35579b
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-02-26 23:57:36 +0800

    更新wensockett

---
+++ core/xianqu/event.go
@@ -0,0 +1,465 @@
+package xianqu
+
+import "C"
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+)
+
+var (
+	AppInfo = newAppInfo()
+
+	OneBotPath = PathExecute() + "/OneBot/"
+
+	OnMessagePrivate      = func(ctx *Context) {}
+	OnMessageGroup        = func(ctx *Context) {}
+	OnNoticeFileUpload    = func(ctx *Context) {}
+	OnNoticeAdminChange   = func(ctx *Context) {}
+	OnNoticeGroupDecrease = func(ctx *Context) {}
+	OnNoticeGroupIncrease = func(ctx *Context) {}
+	OnNoticeGroupBan      = func(ctx *Context) {}
+	OnNoticeFriendAdd     = func(ctx *Context) {}
+	OnNoticeMessageRecall = func(ctx *Context) {}
+	OnRequestFriendAdd    = func(ctx *Context) {}
+	OnRequestGroupAdd     = func(ctx *Context) {}
+	OnEnable              = func(ctx *Context) {}
+
+	MessageIDCache = &CacheData{Max: 1000, Key: []interface{}{}, Value: []interface{}{}}
+	MessageCache   = &CacheData{Max: 1000, Key: []interface{}{}, Value: []interface{}{}}
+	GroupDataCache = &CacheGroupsData{Group: []*GroupData{}}
+	PicPoolCache   = &CacheData{Max: 1000, Key: []interface{}{}, Value: []interface{}{}}
+)
+
+// App XQ要求的插件信息
+type App struct {
+	Name   string `json:"name"`   // 插件名字
+	Pver   string `json:"pver"`   // 插件版本
+	Sver   int    `json:"sver"`   // 框架版本
+	Author string `json:"author"` // 作者名字
+	Desc   string `json:"desc"`   // 插件说明
+}
+
+// newAppInfo 返回插件信息
+func newAppInfo() *App {
+	return &App{
+		Name:   "OneBot-YaYa",
+		Pver:   "1.1.9",
+		Sver:   3,
+		Author: "kanri",
+		Desc:   "OneBot标准的先驱实现 项目地址: http://github.com/Yiwen-Chan/OneBot-YaYa",
+	}
+}
+
+type Context struct {
+	Bot      int64
+	Request  map[string]interface{}
+	Response map[string]interface{}
+}
+
+//export GoCreate
+func GoCreate(version *C.char) *C.char {
+	data, _ := json.Marshal(AppInfo)
+	return CString(string(data))
+}
+
+//export GoSetUp
+func GoSetUp() C.int {
+	return C.int(0)
+}
+
+//export GoDestroyPlugin
+func GoDestroyPlugin() C.int {
+	return C.int(0)
+}
+
+//export GoEvent
+func GoEvent(cBot *C.char, cMessageType, cSubType C.int, cGroupID, cUserID, cNoticeID, cMessage, cMessageNum, cMessageID, cRawMessage, cTime *C.char, cRet C.int) C.int {
+	var (
+		bot         = CStr2GoInt(cBot)
+		messageType = int64(cMessageType)
+		subType     = int64(cSubType)
+		groupID     = CStr2GoInt(cGroupID)
+		userID      = CStr2GoInt(cUserID)
+		noticeID    = CStr2GoInt(cNoticeID)
+		message     = UnescapeEmoji(GoString(cMessage)) // 解决易语言的emoji到utf-8
+		messageNum  = CStr2GoInt(cMessageNum)
+		messageID   = CStr2GoInt(cMessageID)
+		rawMessage  = GoString(cRawMessage)
+		time        = CStr2GoInt(cTime)
+		// ret         = CStr2GoInt(cRet)
+	)
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				XQApiOutPutLog(fmt.Sprintf("[PANIC] 发生了不可预知的错误，请在GitHub提交issue：%v", err))
+				buf := make([]byte, 1<<16)
+				runtime.Stack(buf, true)
+				XQApiOutPutLog(fmt.Sprintf("[TRACEBACK]:\n%v", string(buf)))
+			}
+		}()
+		switch messageType {
+		// 消息事件
+		// 0：临时会话 1：好友会话 4：群临时会话 7：好友验证会话
+		case 1:
+			cqMessage := xq2cqCode(message)
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":         time,
+					"self_id":      bot,
+					"post_type":    "message",
+					"message_type": "private",
+					"sub_type":     "friend",
+					"message_id":   messageID,
+					"user_id":      userID,
+					"message":      cqMessage,
+					"raw_message":  cqMessage,
+					"font":         0,
+					"sender": map[string]interface{}{
+						"user_id":  userID,
+						"nickname": "unknown",
+						"sex":      "unknown",
+						"age":      "unknown",
+					},
+				},
+			}
+			MessageIDCache.Insert(messageID, messageNum)
+			MessageCache.Insert(messageID, ctx.Response)
+			OnMessagePrivate(ctx)
+		// 2：群聊信息
+		case 2:
+			cqMessage := xq2cqCode(message)
+			info := GroupDataCache.GetCacheGroupMember(
+				bot,
+				groupID,
+				userID,
+				true,
+			)
+			sender := map[string]interface{}{
+				"user_id":  userID,
+				"nickname": "unknown",
+				"sex":      "unknown",
+				"age":      "unknown",
+				"area":     "",
+				"card":     "",
+				"level":    "",
+				"role":     "unknown",
+				"title":    "unknown",
+			}
+			if info != nil {
+				sender = map[string]interface{}{
+					"user_id":  userID,
+					"nickname": info.Nickname,
+					"sex":      info.Sex,
+					"age":      info.Sex,
+					"area":     "",
+					"card":     "",
+					"level":    "",
+					"role":     info.Role,
+					"title":    "unknown",
+				}
+			}
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":         time,
+					"self_id":      bot,
+					"post_type":    "message",
+					"message_type": "group",
+					"sub_type":     "normal",
+					"message_id":   messageID,
+					"group_id":     groupID,
+					"user_id":      userID,
+					"anonymous":    nil,
+					"message":      cqMessage,
+					"raw_message":  cqMessage,
+					"font":         0,
+					"sender":       sender,
+				},
+			}
+			MessageIDCache.Insert(messageID, messageNum)
+			MessageCache.Insert(messageID, ctx.Response)
+			OnMessageGroup(ctx)
+		// 10：回音信息
+		case 10:
+			cqMessage := xq2cqCode(message)
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":         time,
+					"self_id":      bot,
+					"post_type":    "message",
+					"message_type": "group",
+					"sub_type":     "normal",
+					"message_id":   messageID,
+					"group_id":     groupID,
+					"user_id":      userID,
+					"anonymous":    nil,
+					"message":      cqMessage,
+					"raw_message":  cqMessage,
+					"font":         0,
+					"sender": map[string]interface{}{
+						"user_id":  userID,
+						"nickname": "unknown",
+						"sex":      "unknown",
+						"age":      0,
+						"area":     "",
+						"card":     "",
+						"level":    "",
+						"role":     "admin",
+						"title":    "unknown",
+					},
+				},
+			}
+			MessageIDCache.Insert(messageID, messageNum)
+			MessageCache.Insert(messageID, ctx.Response)
+		// 通知事件
+		// 群文件接收
+		case 218:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_upload",
+					"group_id":    groupID,
+					"user_id":     userID,
+					"file": map[string]interface{}{
+						"id":    "unknown",
+						"name":  message,
+						"size":  "unknown",
+						"busid": "unknown",
+					},
+				},
+			}
+			OnNoticeFileUpload(ctx)
+		// 管理员变动 210为有人升为管理 211为有人被取消管理
+		case 210:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_admin",
+					"sub_type":    "set",
+					"group_id":    groupID,
+					"user_id":     userID,
+				},
+			}
+			OnNoticeAdminChange(ctx)
+		case 211:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_admin",
+					"sub_type":    "unset",
+					"group_id":    groupID,
+					"user_id":     userID,
+				},
+			}
+			OnNoticeAdminChange(ctx)
+		// 群成员减少 201为某人退出群 202为某人被管理移除群
+		case 201:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_decrease",
+					"sub_type":    "leave",
+					"group_id":    groupID,
+					"operator_id": userID,
+					"user_id":     noticeID,
+				},
+			}
+			OnNoticeGroupDecrease(ctx)
+		case 202:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_decrease",
+					"sub_type":    "kick",
+					"group_id":    groupID,
+					"operator_id": userID,
+					"user_id":     noticeID,
+				},
+			}
+			OnNoticeGroupDecrease(ctx)
+		// 群成员增加
+		case 212:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_increase",
+					"sub_type":    "unknown",
+					"group_id":    groupID,
+					"operator_id": userID,
+					"user_id":     noticeID,
+				},
+			}
+			OnNoticeGroupIncrease(ctx)
+		// 群禁言 203为禁言 204为解禁
+		case 203:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_ban",
+					"sub_type":    "ban",
+					"group_id":    groupID,
+					"operator_id": userID,
+					"user_id":     noticeID,
+					"duration":    "unknown",
+				},
+			}
+			OnNoticeGroupBan(ctx)
+		case 204:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "group_ban",
+					"sub_type":    "lift_ban",
+					"group_id":    groupID,
+					"operator_id": userID,
+					"user_id":     noticeID,
+					"duration":    "unknown",
+				},
+			}
+			OnNoticeGroupBan(ctx)
+		// new
+		// 好友添加 100 为单向 102 为标准
+		case 100, 102:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":        time,
+					"self_id":     bot,
+					"post_type":   "notice",
+					"notice_type": "friend_add",
+					"user_id":     userID,
+				},
+			}
+			OnNoticeFriendAdd(ctx)
+		// 群消息撤回 subType 2
+		// 好友消息撤回 subType 1
+		case 9:
+			switch subType {
+			case 1:
+				ctx := &Context{
+					Bot: bot,
+					Response: map[string]interface{}{
+						"time":        time,
+						"self_id":     bot,
+						"post_type":   "notice",
+						"notice_type": "group_recall",
+						"group_id":    groupID,
+						"user_id":     noticeID,
+						"operator_id": userID,
+						"message_id":  messageID,
+					},
+				}
+				OnNoticeMessageRecall(ctx)
+			case 2:
+				ctx := &Context{
+					Bot: bot,
+					Response: map[string]interface{}{
+						"time":        time,
+						"self_id":     bot,
+						"post_type":   "notice",
+						"notice_type": "friend_recall",
+						"group_id":    groupID,
+						"user_id":     noticeID,
+						"operator_id": userID,
+						"message_id":  messageID,
+					},
+				}
+				OnNoticeMessageRecall(ctx)
+			}
+
+		// 群内戳一戳
+
+		// 群红包运气王
+
+		// 群成员荣誉变更
+
+		// 请求事件
+		// 加好友请求
+		case 101:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":         time,
+					"self_id":      bot,
+					"post_type":    "request",
+					"request_type": "friend",
+					"user_id":      noticeID,
+					"comment":      message,
+					"flag":         userID,
+				},
+			}
+			OnRequestFriendAdd(ctx)
+		// 加群请求／邀请 213为请求 214为被邀
+		case 213:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":      time,
+					"self_id":   bot,
+					"post_type": "request",
+					"sub_type":  "add",
+					"group_id":  groupID,
+					"user_id":   noticeID,
+					"comment":   message,
+					"flag":      fmt.Sprintf("%v|%v|%v", subType, groupID, rawMessage),
+				},
+			}
+			OnRequestGroupAdd(ctx)
+		case 214:
+			ctx := &Context{
+				Bot: bot,
+				Response: map[string]interface{}{
+					"time":      time,
+					"self_id":   bot,
+					"post_type": "request",
+					"sub_type":  "invite",
+					"group_id":  groupID,
+					"user_id":   noticeID,
+					"comment":   message,
+					"flag":      fmt.Sprintf("%v|%v|%v", subType, groupID, rawMessage),
+				},
+			}
+			OnRequestGroupAdd(ctx)
+		case 10000:
+			XQApiOutPutLog(`  ____  __     _ _____________    ____ _________ `)
+			XQApiOutPutLog(` / _   \|  \   |   |   ____|   __  ) /  __  |___   ___| `)
+			XQApiOutPutLog(`|  |   |  |    \ |  |   __|  |   __  \|   |   |  |  |  |       `)
+			XQApiOutPutLog(`|  |__|   |  | \    |   |__ _|   |_)   |   |__|  |  |  |      `)
+			XQApiOutPutLog(` \____/ |_ |  \ _|______|______/ \_____/   |_ |     `)
+			ctx := &Context{
+				Bot: bot,
+			}
+			OnEnable(ctx)
+		default:
+			//
+		}
+	}()
+	return 0
+}
--- server/websocket_server.go
+++ server/websocket_server.go
@@ -2,137 +2,209 @@ package server
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
+	"runtime"
 	"strconv"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-type WebSocketServer struct {
-	ID                int64  `yaml:"-"`
-	Name              string `yaml:"name"`
-	Enable            bool   `yaml:"enable"`
-	Host              string `yaml:"host"`
-	Port              int64  `yaml:"port"`
-	AccessToken       string `yaml:"access_token"`
-	PostMessageFormat string `yaml:"post_message_format"`
-	HeartBeatInterval int64  `yaml:"reconnect_interval"`
-
-	ConnectStatus string `yaml:"-"`
-
-	Connects []Connect `yaml:"-"`
-}
-
-type Connect struct {
-	Conn *websocket.Conn `yaml:"-"`
-
-	StopSend      chan bool `yaml:"-"`
-	StopHeartBeat chan bool `yaml:"-"`
-
-	ListenStatus    string `yaml:"-"`
-	SendStatus      string `yaml:"-"`
-	HeartBeatStatus string `yaml:"-"`
-
-	SendChan chan []byte `yaml:"-"`
-}
-
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
 
-func (this *WebSocketServer) Connect() {
-	this.ConnectStatus = "connect"
-	http.ListenAndServe(fmt.Sprintf("%v:%v", this.Host, this.Port), this)
-	this.ConnectStatus = "ok"
+func (s *WebSocketServer) Init() {
+
+	if s.Conn == nil {
+		s.Conn = []*WebSocketServerConn{}
+	}
 }
 
-func (this *WebSocketServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *WebSocketServer) Run() {
+	switch {
+	case s.Enable && s.ConnectStatus == S_WAIT && s.Host != "":
+		if s.Port == 0 {
+			s.Port = 80
+		}
+		INFO(s, "Listen Start")
+		go s.Connect()
+		fallthrough
+	case s.Enable && s.ConnectStatus == S_OK:
+		for i := range s.Conn {
+			switch {
+			case s.Conn[i].ListenStatus == S_WAIT:
+				go s.Listen(s.Conn[i])
+				INFO(s, "Listen Start")
+			case s.Conn[i].SendStatus == S_WAIT:
+				go s.Send(s.Conn[i])
+				INFO(s, "Send Start")
+			}
+		}
+	}
+}
+
+func (s *WebSocketServer) Stop(force bool) {
+	switch {
+	case s.ConnectStatus == S_ERROR:
+		if s.Server != nil {
+			s.Server.Close()
+		}
+		time.Sleep(time.Second * 1)
+		s.ConnectStatus = S_WAIT
+		fallthrough
+	case s.ConnectStatus == S_OK:
+		for i := range s.Conn {
+			switch {
+			case s.Conn[i].ListenStatus == S_ERROR:
+				if s.Conn[i].SendStatus == S_OK {
+					s.Conn[i].StopSend <- true
+				}
+				if s.Conn[i].Conn != nil {
+					s.Conn[i].Conn.Close()
+					s.Conn[i].Conn = nil
+				}
+				time.Sleep(time.Second * 1)
+				s.Mutex.Lock()
+				s.Conn = append(s.Conn[:i], s.Conn[i+1:]...)
+				s.Mutex.Unlock()
+			case s.Conn[i].SendStatus == S_ERROR:
+				if s.Conn[i].Conn != nil {
+					s.Conn[i].Conn.Close()
+					s.Conn[i].Conn = nil
+				}
+				time.Sleep(time.Second * 1)
+				s.Mutex.Lock()
+				s.Conn = append(s.Conn[:i], s.Conn[i+1:]...)
+				s.Mutex.Unlock()
+			}
+		}
+	case force:
+		for i := range s.Conn {
+			switch {
+			case s.Conn[i].SendStatus == S_OK:
+				s.Conn[i].StopSend <- true
+			case s.Conn[i].ListenStatus == S_OK:
+				if s.Conn[i].Conn != nil {
+					s.Conn[i].Conn.Close()
+					s.Conn[i].Conn = nil
+				}
+			}
+		}
+		time.Sleep(time.Second * 1)
+		s.Mutex.Lock()
+		s.Conn = nil
+		s.Mutex.Unlock()
+		if s.Server != nil {
+			s.Server.Close()
+			s.Server = nil
+		}
+	}
+}
+
+func (s *WebSocketServer) Connect() {
+	// 开始监听
+	s.ConnectStatus = S_OK
 	defer func() {
-		recover()
+		s.ConnectStatus = S_WAIT
+		if err := recover(); err != nil {
+			// 建立监听时或监听过程发生错误
+			s.ConnectStatus = S_ERROR
+			ERROR(s, err)
+		}
 	}()
-	if !(r.Header.Get("Authorization") == "Token "+this.AccessToken || this.AccessToken == "") {
-		panic(errors.New("token error"))
+	s.ConnectStatus = S_OK
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", s.Host, s.Port),
+		Handler: s,
+	}
+	s.Server = server
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func (s *WebSocketServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !(r.Header.Get("Authorization") == s.AccessToken || s.AccessToken == "") {
+		// Token验证失败
+		ERROR(s, "Token ERROR")
+		return
 	}
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		panic(err)
+		ERROR(s, err)
+		return
 	}
 	// 元事件 表示OneBot启动成功
 	// https://github.com/howmanybots/onebot/blob/master/v11/specs/event/meta.md#%E7%94%9F%E5%91%BD%E5%91%A8%E6%9C%9F
 	handshake, _ := json.Marshal(map[string]string{
 		"meta_event_type": "lifecycle",
 		"post_type":       "meta_event",
-		"self_id":         strconv.FormatInt(this.ID, 10),
+		"self_id":         strconv.FormatInt(s.ID, 10),
 		"sub_type":        "connect",
 		"time":            strconv.FormatInt(time.Now().Unix(), 10),
 	})
-	_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 15))
 	if err := conn.WriteMessage(websocket.TextMessage, handshake); err != nil {
-		panic(err)
+		ERROR(s, err)
+		return
 	}
-	this.Connects = append(this.Connects, Connect{
-		Conn:            conn,
-		StopSend:        make(chan bool, 1),
-		StopHeartBeat:   make(chan bool, 1),
-		ListenStatus:    "wait",
-		SendStatus:      "wait",
-		HeartBeatStatus: "wait",
-		SendChan:        make(chan []byte, 50),
+	s.Mutex.Lock()
+	defer s.Mutex.Unlock()
+	s.Conn = append(s.Conn, &WebSocketServerConn{
+		Conn:         conn,
+		StopSend:     make(chan bool, 1),
+		ListenStatus: S_WAIT,
+		SendStatus:   S_WAIT,
+		SendChan:     make(chan []byte, 100),
+		Handler:      WebSocketServerHandler,
 	})
 }
 
-func (this *WebSocketServer) Listen(connect Connect) {
-	connect.ListenStatus = "ok"
+func (s *WebSocketServer) Listen(connect *WebSocketServerConn) {
+	connect.ListenStatus = S_OK
 	defer func() {
-		connect.ListenStatus = "wait"
-		recover()
+		connect.ListenStatus = S_WAIT
+		if err := recover(); err != nil {
+			// 读取信息或Handler传递时发生错误
+			connect.ListenStatus = S_ERROR
+			ERROR(s, err)
+			buf := make([]byte, 1<<16)
+			runtime.Stack(buf, true)
+			ERROR(s, fmt.Sprintf("[TRACEBACK]:\n%v", string(buf)))
+		}
 	}()
 	for {
 		_, buf, err := connect.Conn.ReadMessage()
 		if err != nil {
 			panic(err)
 		}
-		ret := CallApi("WSS", buf)
+		ret := connect.Handler(s.ID, buf)
 		connect.SendChan <- ret
 	}
 }
 
-func (this *WebSocketServer) Send(connect Connect) {
-	connect.SendStatus = "ok"
+func (s *WebSocketServer) Send(connect *WebSocketServerConn) {
+	connect.SendStatus = S_OK
 	defer func() {
-		connect.SendStatus = "wait"
-		recover()
-	}()
-	for {
-		select {
-		case send := <-connect.SendChan:
-			if err := connect.Conn.WriteMessage(websocket.TextMessage, send); err != nil {
-				panic(err)
-			}
-		case <-connect.StopSend:
-			return
+		connect.SendStatus = S_WAIT
+		if err := recover(); err != nil {
+			connect.SendStatus = S_ERROR
+			ERROR(s, err)
 		}
-	}
-}
-
-func (this *WebSocketServer) HeartBeat(connect Connect) {
-	connect.HeartBeatStatus = "ok"
-	defer func() {
-		connect.HeartBeatStatus = "wait"
-		recover()
 	}()
 	for {
 		select {
-		case <-time.After(time.Second * time.Duration(this.HeartBeatInterval)):
+		case <-connect.StopSend:
+			ERROR(s, "Send 命令退出")
+			return
+		case <-time.After(time.Millisecond * time.Duration(s.HeartBeatInterval)):
 			// OneBot标准 元事件 心跳
 			// https://github.com/howmanybots/onebot/blob/master/v11/specs/event/meta.md#%E5%BF%83%E8%B7%B3
 			heartbeat, _ := json.Marshal(
 				map[string]interface{}{
-					"interval":        strconv.FormatInt(this.HeartBeatInterval, 10),
+					"interval":        strconv.FormatInt(s.HeartBeatInterval, 10),
 					"meta_event_type": "heartbeat",
 					"post_type":       "meta_event",
-					"self_id":         strconv.FormatInt(this.ID, 10),
+					"self_id":         strconv.FormatInt(s.ID, 10),
 					"status": map[string]interface{}{
 						"online": true,
 						"good":   true,
@@ -141,8 +213,11 @@ func (this *WebSocketServer) HeartBeat(connect Connect) {
 				},
 			)
 			connect.SendChan <- heartbeat
-		case <-connect.StopHeartBeat:
-			return
+		case send := <-connect.SendChan:
+			if err := connect.Conn.WriteMessage(websocket.TextMessage, send); err != nil {
+				panic(err)
+			}
+
 		}
 	}
 }
