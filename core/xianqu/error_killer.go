292b0f867292562d120cd6a5fafd8a4e1fb0c6ff
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-04-08 21:24:21 +0800

    ✨ 增加错误弹框自动关闭

---
+++ core/xianqu/error_killer.go
@@ -0,0 +1,22 @@
+package xianqu
+
+/*
+#include <string.h>
+#include <windows.h>
+#include <tchar.h>
+void kill(char *text) {
+	TCHAR ch[100];
+    _stprintf(ch, TEXT("%s"), text);
+    SendMessage(FindWindow(NULL,ch),WM_CLOSE,0,0);
+    return;
+}
+*/
+import "C"
+import "time"
+
+func errorkiller() {
+	for {
+		time.Sleep(time.Second * 5)
+		C.kill(CString("先驱框架运行时出现异常！"))
+	}
+}
